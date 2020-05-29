2644baca58d670cb09729af906ecfa3d3f90be09
Cory Binnersley <corybinnersley@gmail.com>
2020-05-28 19:22:27 -0700

    Fix typo

--- a2c_ppo_acktr/algo/ppo.py
+++ a2c_ppo_acktr/algo/ppo.py
@@ -40,7 +40,7 @@ class PPO():
         action_loss_epoch = 0
         dist_entropy_epoch = 0
 
-        for e in tdqm(range(self.ppo_epoch)):
+        for e in tqdm(range(self.ppo_epoch)):
             if self.actor_critic.is_recurrent:
                 data_generator = rollouts.recurrent_generator(
                     advantages, self.num_mini_batch)
